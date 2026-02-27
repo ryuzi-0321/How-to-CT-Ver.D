ec7e1092f91e9c0e669ef880d4a5ca731479fcf6
ryuzi-0321 <baseballbear0321@gmail.com>
2026-02-27 23:12:24 +0900

    setting.pyをfalseに変更

--- medical_ct_project/settings.py
+++ medical_ct_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ct-app-development-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', '{houka362}.pythonanywhere.com'
 ]
